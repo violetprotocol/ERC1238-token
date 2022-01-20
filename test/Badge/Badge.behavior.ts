da2931005e467f37673a0e8191b95a7f33386367
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-01-20 17:31:36 +0000

    move badge testing file

--- test/Badge/Badge.behavior.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from "chai";
-
-export function shouldBehaveLikeBadge(): void {
-  it("should return the right owner address", async function () {
-    expect(await this.Badge.connect(this.signers.admin).owner()).to.equal(this.signers.admin.address);
-  });
-}
