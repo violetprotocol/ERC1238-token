c093348cb6253d2c24a4237a4d05c06bed9fdfc9
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-01-20 12:09:53 +0000

    add tooling

---
+++ test/types.ts
@@ -0,0 +1,16 @@
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import type { Fixture } from "ethereum-waffle";
+
+import type { Badge } from "../src/types/Badge";
+
+declare module "mocha" {
+  export interface Context {
+    badge: Badge;
+    loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
+    signers: Signers;
+  }
+}
+
+export interface Signers {
+  admin: SignerWithAddress;
+}
