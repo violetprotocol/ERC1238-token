2f9fc6585ae3d593096988f80f17e3f2579d4db7
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-01-28 10:52:49 +0000

    minor change

--- contracts/ERC1238/extensions/IERC1238URIStorage.sol
+++ contracts/ERC1238/extensions/IERC1238URIStorage.sol
@@ -8,17 +8,14 @@ import "../IERC1238.sol";
  * @dev Proposal of an interface for ERC1238 token with storage based token URI management.
  */
 interface IERC1238URIStorage is IERC1238 {
-
     /**
-    * @dev Returns the Uniform Resource Identifier (URI) for `id` token.
-    */
-    function tokenURI(uint256 id) external view returns (string memory);
-
-
-     /**
      * @dev Emitted when the URI for token type `id` changes to `value`, if it is a non-programmatic URI.
      *
      */
     event URI(uint256 indexed id, string value);
 
+    /**
+     * @dev Returns the Uniform Resource Identifier (URI) for `id` token.
+     */
+    function tokenURI(uint256 id) external view returns (string memory);
 }
