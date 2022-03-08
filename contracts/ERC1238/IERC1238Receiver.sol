a4fef97dd2bd87e736ad65bffe41549886c4b8a1
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-03-08 21:07:47 +0000

    small fix

--- contracts/ERC1238/IERC1238Receiver.sol
+++ contracts/ERC1238/IERC1238Receiver.sol
@@ -29,7 +29,7 @@ interface IERC1238Receiver {
      * @dev Handles the receipt of multiple ERC1238 token types.
      *
      * NOTE: To accept the transfer(s), this must return
-     * `bytes4(keccak256("onERC1238BatchMint(address,address,uint256[],uint256[],bytes)"))`
+     * `bytes4(keccak256("onERC1238BatchMint(address,uint256[],uint256[],bytes)"))`
      *
      * @param minter The address which initiated minting (i.e. msg.sender)
      * @param ids An array containing ids of each token being transferred (order and length must match values array)
