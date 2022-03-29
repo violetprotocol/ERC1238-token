721a3e2dcc3a53677bdf689df10cbaa7ffd9d3fa
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-03-29 13:52:56 +0100

    add ERC1238ReceiverMock

---
+++ contracts/mocks/ERC1238ReceiverMock.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../ERC1238/IERC1238Receiver.sol";
+import "hardhat/console.sol";
+
+contract ERC1238ReceiverMock is IERC1238Receiver {
+    bytes4 public constant ERC1238_ON_MINT = bytes4(keccak256("onERC1238Mint(address,uint256,uint256,bytes)"));
+    bytes4 public constant ERC1238_ON_BATCH_MINT =
+        bytes4(keccak256("onERC1238BatchMint(address,uint256[],uint256[],bytes)"));
+
+    function onERC1238Mint(
+        address minter,
+        uint256 id,
+        uint256 amount,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        console.log("token id: %s", id);
+        if (id == 0) {
+            return bytes4(0);
+        }
+
+        return ERC1238_ON_MINT;
+    }
+
+    function onERC1238BatchMint(
+        address minter,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        if (ids[0] == 0) {
+            return bytes4(0);
+        }
+
+        return ERC1238_ON_BATCH_MINT;
+    }
+}
