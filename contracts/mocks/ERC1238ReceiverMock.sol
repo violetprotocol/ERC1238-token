cdd16ac8ad9bec27e9a8f83725728c579ea3d1ee
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-04-01 09:58:36 +0100

    add comment

--- contracts/mocks/ERC1238ReceiverMock.sol
+++ contracts/mocks/ERC1238ReceiverMock.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.0;
 
 import "../ERC1238/IERC1238Receiver.sol";
 
+// This is a dummy example of a ERC1238Receiver with arbitrary rules.
+// It will reject non-transferable tokens if the token id is 0 in the case of a single token mint
+// or if the first token id is 0 for a batch mint.
 contract ERC1238ReceiverMock is IERC1238Receiver {
     //  bytes4(keccak256("onERC1238Mint(address,uint256,uint256,bytes)"))
     bytes4 public constant ERC1238_ON_MINT = 0x45ed75d5;
